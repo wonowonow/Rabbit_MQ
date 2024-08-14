f1c1af6a516d71a1da08e516e9ea5f9af5876b83
wonowonow <wonow_@naver.com>
2024-08-14 16:47:44 +0900

    Listener 만들기~

---
+++ payment/src/main/java/com/market/payment/PaymentEndpoint.java
@@ -0,0 +1,20 @@
+package com.market.payment;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PaymentEndpoint {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RabbitListener(queues = "${message.queue.payment}")
+    public void receiveMessage(String orderId) {
+        log.info("Received orderId: {} appName: {}", orderId, appName);
+
+    }
+}
