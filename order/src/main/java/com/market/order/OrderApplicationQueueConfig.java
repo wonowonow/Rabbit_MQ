95b64ec5e10f9b2e710f507eb6efca5b298fd3dc
wonowonow <wonow_@naver.com>
2024-08-14 16:10:39 +0900

    사용하지 않는 import 삭제

--- order/src/main/java/com/market/order/OrderApplicationQueueConfig.java
+++ order/src/main/java/com/market/order/OrderApplicationQueueConfig.java
@@ -2,7 +2,6 @@ package com.market.order;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
